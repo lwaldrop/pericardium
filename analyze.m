017d08b7ecaee26ccdfbc56e499888ae65b83f78
Lindsay Waldrop <lwaldrop@email.unc.edu>
2015-05-21 11:56:33 -0400

    Adding pressure analysis code to analyze.m

--- analyze.m
+++ analyze.m
@@ -5,19 +5,27 @@ clear all
 
 
 % loads data
-a = load('ave_vel_test');
+aveVel = load('ave_vel_test');
+pressLine = load('press_line_test');
 numpts = 27;
 mindist=50;
 
 % Calculates number of time steps
-n = length(a)/numpts;
+n = length(aveVel)/numpts;
 
 % Allocates space
-x = zeros(n,numpts);
-y = zeros(n,numpts);
-peakx = zeros(n,1);
-peaky = zeros(n,1);
-meany = zeros(n,1);
+xVel = zeros(n,numpts);
+yVel = zeros(n,numpts);
+peakxVel = zeros(n,1);
+peakyVel = zeros(n,1);
+meanyVel = zeros(n,1);
+
+xPress = zeros(n,numpts);
+yPress = zeros(n,numpts);
+peakxPress = zeros(n,1);
+peakyPress = zeros(n,1);
+meanyPress = zeros(n,1);
+
 
 figure(1)
 hold on
@@ -25,50 +33,103 @@ hold on
 
 be =0;
 
-for i = 1:n
+%Calculations for Average Velocity.
+for i = 1:n;
      
     bs = be+1;
     be = bs+(numpts-1);
-    x(i,:) = a(bs:be,2);            % Selects the time for this step
-    y(i,:) = a(bs:be,1);            % Selects all velocities for step.
+    xVel(i,:) = aveVel(bs:be,2);            % Selects the time for this step
+    yVel(i,:) = aveVel(bs:be,1);            % Selects all velocities for step.
     
-    [pk,loc] = max(abs(y(i,:)));    % Finds index of peak speed 'loc'
-    peaky(i,1) = y(i,loc);          % Uses loc to find speed value.
-    peakx(i,1) = max(x(i,:));            % Finds time for current step.
+    [pk,loc] = max(abs(yVel(i,:)));    % Finds index of peak speed 'loc'
+    peakyVel(i,1) = yVel(i,loc);          % Uses loc to find speed value.
+    peakxVel(i,1) = max(xVel(i,:));            % Finds time for current step.
     
-    meany(i,1) = mean(y(i,:));
+    meanyVel(i,1) = mean(yVel(i,:));
     
     % plots all velocities for this time step on one graph.
-    plot(y(i,:))
+    plot(yVel(i,:))
     
 end
 
 hold off
 
-grand_mean = mean(meany)
+grand_meanVel = mean(meanyVel)
 
 figure(2)
 
-plot(peakx,peaky)
+plot(peakxVel,peakyVel)
 hold on
-plot(peakx,meany,'r-')
+plot(peakxVel,meanyVel,'r-')
 
-[pks,local] = findpeaks(peaky,'minpeakdistance',mindist)
+[pks,local] = findpeaks(peakyVel,'minpeakdistance',mindist);
  
-plot(peakx(local),peaky(local),'r*')
+plot(peakxVel(local),peakyVel(local),'r*')
 
 hold off
 
-tiny_mean=mean(pks)
+tiny_meanVel=mean(pks)
 
 %State what was found:
 
 
 
 % Saves peak data so it can be plotted in R.
-fid = fopen('peak_data.csv', 'w') ;
-fprintf(fid,'%s\n','peakdata') ;
+fid = fopen('peak_Veldata.csv', 'w') ;
+fprintf(fid,'%s\n','peakVeldata') ;
 fprintf(fid, '%s\n') ;
 fclose(fid) ;
-dlmwrite('peak_data.csv', peaky, '-append') ;
+dlmwrite('peakVel_data.csv', peakyVel, '-append') ;
+
+clear bs be i pks local loc pk
+
+be =0;
+
+%Calculations for Pressure.
+for i = 1:n
+     
+    bs = be+1;
+    be = bs+(numpts-1);
+    xPress(i,:) = pressLine(bs:be,2);            % Selects the time for this step
+    yPress(i,:) = pressLine(bs:be,1);            % Selects all velocities for step.
+    
+    [pk,loc] = max(abs(yPress(i,:)));    % Finds index of peak speed 'loc'
+    peakyPress(i,1) = yPress(i,loc);          % Uses loc to find speed value.
+    peakxPress(i,1) = max(xPress(i,:));            % Finds time for current step.
+    
+    meanyPress(i,1) = mean(yPress(i,:));
+    
+    % plots all velocities for this time step on one graph.
+    plot(yPress(i,:))
+    
+end
+
+hold off
+
+grand_meanPress = mean(meanyPress)
+
+figure(2)
+
+plot(peakxPress,peakyPress)
+hold on
+plot(peakxPress,meanyPress,'r-')
+
+[pksPress,local] = findpeaks(peakyPress,'minpeakdistance',mindist);
+ 
+plot(peakxPress(local),peakyPress(local),'r*')
+
+hold off
+
+tiny_meanPress=mean(pksPress)
+
+%State what was found:
+
+
+
+% Saves peak data so it can be plotted in R.
+fid = fopen('peakPress_data.csv', 'w') ;
+fprintf(fid,'%s\n','peakPressdata') ;
+fprintf(fid, '%s\n') ;
+fclose(fid) ;
+dlmwrite('peakPress_data.csv', peakyPress, '-append') ;
 
